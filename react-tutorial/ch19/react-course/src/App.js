c72264f0e712f0aab7ead1b5f2b6cb2d6ff2ac1a
woongcheol Shin <5starshin@gmail.com>
2023-07-17 16:27:54 +0900

    feat: Todo 컴포넌트 제작

---
+++ react-tutorial/ch19/react-course/src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Learn JavaScript" />
+      <Todo text="Learn Programming" />
+    </div>
+  );
+}
+
+export default App;
