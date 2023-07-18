97b7453fa5a7ce5a115e30e2447dd2a1e9f7d98e
woongcheol Shin <5starshin@gmail.com>
2023-07-18 13:49:40 +0900

    feat: nav bar 추가

--- react-tutorial/ch34/react-course/src/components/layout/MainNavigation.js
+++ react-tutorial/ch34/react-course/src/components/layout/MainNavigation.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
+
 function MainNavigation() {
   return (
-    <header>
-      <div>All Meeting</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>Meeting</div>
       <nav>
         <ul>
           <li>
