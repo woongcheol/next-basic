f236680a6d25301d00159162918cacb7decf276b
woongcheol Shin <5starshin@gmail.com>
2023-07-18 17:18:47 +0900

    feat: firebase 연동 및 데이터 송수신 api 적용

--- react-tutorial/ch34/react-course/src/components/meetups/NewMeetupForm.js
+++ react-tutorial/ch34/react-course/src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import classes from "./NewMeetupForm.module.css";
 import Card from "../ui/Card";
 import { useRef } from "react";
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -22,7 +22,7 @@ function NewMeetupForm() {
       description: enteredDescription,
     };
 
-    console.log(meetupData);
+    props.onAddMeetup(meetupData);
   }
 
   return (
--- react-tutorial/ch34/react-course/src/pages/NewMeetup.js
+++ react-tutorial/ch34/react-course/src/pages/NewMeetup.js
@@ -1,10 +1,28 @@
 import NewMeetupFrom from "../components/meetups/NewMeetupForm";
+import { useHistory } from "react-router-dom";
 
 function NewMeetupPage() {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://next-udemy-tutorial-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        header: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupFrom />
+      <NewMeetupFrom onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
