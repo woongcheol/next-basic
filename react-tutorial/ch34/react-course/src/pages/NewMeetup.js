e1f73799678822f4dc77da6c3f7e813ce1b4d2d9
woongcheol Shin <5starshin@gmail.com>
2023-07-18 16:11:42 +0900

    feat: meetup form 컴포넌트 제작 및 적용

--- react-tutorial/ch34/react-course/src/pages/NewMeetup.js
+++ react-tutorial/ch34/react-course/src/pages/NewMeetup.js
@@ -1,5 +1,12 @@
+import NewMeetupFrom from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-  return <div>NewMeetup Page</div>;
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupFrom />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
