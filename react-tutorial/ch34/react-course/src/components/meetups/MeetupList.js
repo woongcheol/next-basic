8266a2bf431bd918684bcc96bfa71bf7e0679a95
woongcheol Shin <5starshin@gmail.com>
2023-07-19 09:59:42 +0900

    feat: favorites 기능 추가

--- react-tutorial/ch34/react-course/src/components/meetups/MeetupList.js
+++ react-tutorial/ch34/react-course/src/components/meetups/MeetupList.js
@@ -7,6 +7,7 @@ function MeetupList(props) {
       {props.meetups.map((meetup) => (
         <MeetupItem
           key={meetup.id}
+          id={meetup.id}
           image={meetup.image}
           title={meetup.title}
           address={meetup.address}
